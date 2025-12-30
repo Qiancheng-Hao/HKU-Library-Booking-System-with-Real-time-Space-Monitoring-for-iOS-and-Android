50beebd1be131ea2fecd4343d22eeb00052d01da
Qiancheng HAO <qianchenghao398@gmail.com>
2025-12-30 20:09:37 +0800

    solve unexpected msg appear after token expired

--- frontend/lib/screens/login_screen.dart
+++ frontend/lib/screens/login_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../services/api_service.dart';
 import 'register_screen.dart';
 import '../providers/auth_provider.dart';
 
